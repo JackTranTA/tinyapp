182b346a882facb03d62ee24faf03c157abc856a
Jack Tran <bluestar113311@gmail.com>
2023-08-12 23:41:06 -0700

    Refactored express_server.js to use exported database objects from datacase.js

---
+++ database.js
@@ -0,0 +1,24 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: "123",
+  },
+  aJasfE: {
+    id: "uaJasfE",
+    email: "user2@example.com",
+    password: "321",
+  }};
+
+  module.exports = { urlDatabase, users };
--- express_server.js
+++ express_server.js
@@ -23,28 +23,7 @@ const { getUserByEmail, urlsForUser, generateRandomString  } = require('./helper
 
 // -------------------- Data --------------------//
 
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
-
-const users = {
-  aJ48lW: {
-    id: "aJ48lW",
-    email: "user@example.com",
-    password: "123",
-  },
-  aJasfE: {
-    id: "uaJasfE",
-    email: "user2@example.com",
-    password: "321",
-  }};
+const { urlDatabase, users } = require('./database.js');
 
 //-------------------- GET Routes --------------------//
 
